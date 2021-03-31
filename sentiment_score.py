100ffb28e78831c2c6ce5567e34b309f53eabd34
Nick Sileo <sileonick@gmail.com>
2021-03-30 21:11:11 -0400

    Created lemmaAdder function
    Takes in a dictionary of words as keys and a list of emotions as their values and returns a dict with those words, plus their stems. The stem entries inherit the value of the first word that stems to it.

--- sentiment_score.py
+++ sentiment_score.py
@@ -7,27 +7,30 @@ from NRCLexPkg.nrclex import NRCLex
 
 df = pandas.read_csv('mypersonality_final.csv',
                      header=0)
-print(df.head())
+#print(df.head())
 test = "This is bad news! I am hopeful to see how it turns out. I am anxious"
 test_bad = "This was not that badly good for me! i could not resist"
 test_emoticons = "0.0"
 
 
 
+
+noEmotes = []
 no_emotion = 0
 for status_update in df["STATUS"]:
     # print(analyzer.polarity_scores(status_update))
     emotions_object = NRCLex(status_update)
-    print(emotions_object.raw_emotion_scores)
+    #print(emotions_object.raw_emotion_scores)
     if len(emotions_object.raw_emotion_scores) == 0:
         no_emotion += 1
+        noEmotes.append(emotions_object.text)
 
-print(no_emotion / len(df["STATUS"]))
+#print(no_emotion / len(df["STATUS"]))
 
-emotions_object_test = NRCLex(test)
-print(emotions_object_test.raw_emotion_scores)
+#emotions_object_test = NRCLex(test)
+#print(emotions_object_test.raw_emotion_scores)
 
 
-print("\ntest_emoticons: ")
-emotions_object_test_emoticons = NRCLex(test_emoticons)
-print(emotions_object_test_emoticons.raw_emotion_scores)
+#print("\ntest_emoticons: ")
+#emotions_object_test_emoticons = NRCLex(test_emoticons)
+#print(emotions_object_test_emoticons.raw_emotion_scores)
