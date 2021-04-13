fd219654b46db0046e67af1d77a5f80310107e08
Nick Sileo <sileonick@gmail.com>
2021-04-13 15:54:45 -0400

    Added negations skeleton

---
+++ negations.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 13 12:45:33 2021
+
+@author: nicks
+"""
+
+
+stat0 = "Where is the negation in this sentence?"
+stat1 = "is having a bad day. not even target, shoes, nor a good guacamole carls jr. burger could bring me up."
+stat2 = "UGGGGGGHHHHH! My WoW account is totally screwed up and now they've \"permanently disabled\" it. So not cool!"
+stat3 = "just found out Chinese New Year is on Valentine's Day... that makes two holidays I have no one to celebrate with... and no, I'm not sulking"
+stat4 = "People should not encourage me to drink until 4 o'clock in the morning :("
+stat5 = "I like to drink until 4 in the morning, but it is not good for me"
+
+neg_words = ["no", "not", "rather", "couldn’t", "wasn’t", "didn’t", "wouldn’t",
+             "shouldn’t", "weren’t", "don’t", "doesn’t", "haven’t", "hasn’t", 
+             "won’t", "wont", "hadn’t", "never", "none", "nobody", "nothing",
+             "neither", "nor", "nowhere", "isn’t", "can’t", "cannot", "mustn’t",
+             "mightn’t", "shan’t", "without", "needn’t"]
+
+NEGATE = [ "aint", "arent", "cannot", "cant",
+        "couldnt", "darent", "didnt", "doesnt",
+        "ain't", "aren't", "can't", "couldn't",
+        "daren't", "didn't", "doesn't", "dont",
+        "hadnt", "hasnt", "havent", "isnt",
+        "mightnt", "mustnt", "neither", "don't",
+        "hadn't", "hasn't", "haven't", "isn't",
+        "mightn't", "mustn't", "neednt", "needn't",
+        "never", "none", "nope", "nor",
+        "not", "nothing", "nowhere", "oughtnt",
+        "shant", "shouldnt", "uhuh", "wasnt",
+        "werent", "oughtn't", "shan't", "shouldn't",
+        "uh-uh", "wasn't", "weren't", "without",
+        "wont", "wouldnt", "won't", "wouldn't",
+        "rarely", "seldom", "despite"]
+
+conj_cond = ["if", "unless", "when"]
+conj_addComp = ["and", "moreover", "additionally", "further",
+             "furthermore", "also", "plus"]
+conj_cont = ["but", "however", "instead"]
+
+import pandas as pd
+import punctuation_analyzer
+import nltk
+
+from NRCLexPkg.nrclex_base import NRCLex
+
+df = pd.read_csv('mypersonality_final.csv', header=0)
+
+def negationAnalyzer(status):
+    #Tokenize the status
+    stat = nltk.word_tokenize(status)
+    
+    #Checks for negation words in the tokens
+    hasNeg = True in (tok in neg_words for tok in stat)
+    if hasNeg == True:
+        #This status has a negation word
+        print("Contains Negation Word")
+        
+        #Cut out first and last tokens- they do not matter if they are conjunctions
+        subStat = stat[1:-1]
+        idealPos = len(subStat)/2
+        
+        #Need to check for conjunctions
+        conjType = "None"
+        conjLoc = -1
+        curPos = 0
+        bestDist = 99
+        
+        #Gets counts of each, finds the one closest to middle to be the priority conjunction
+        for word in subStat:
+            if word in conj_cond:
+                if abs(curPos-idealPos) < bestDist:
+                    conjType = "cond"
+                    bestDist = abs(curPos-idealPos)
+                    conjLoc = curPos
+                    
+            if word in conj_addComp:
+                if abs(curPos-idealPos) < bestDist:
+                    conjType = "addComp"
+                    bestDist = abs(curPos-idealPos)
+                    conjLoc = curPos
+                    
+            if word in conj_cont:
+                if abs(curPos-idealPos) < bestDist:
+                    conjType = "cont"
+                    bestDist = abs(curPos-idealPos)
+                    conjLoc = curPos
+                    
+            curPos += 1
+            
+        #Now conj type of the centralmost conjunction is found
+        #NOTE- conjLoc is its index inside the subStat- NOT stat
+        ##print(conjType)
+        print(conjLoc)
+        
+        if conjType == "None":
+            #trigram stuff
+            print("Trigram")
+        elif conjType == "cond":
+            #Conditional
+            #P(S) = P(Cond, C) + P(Cons.C)
+            print("Cond")
+
+        elif conjType == "addComp":
+            #Additive or Comparative type
+            #P = P(RP) + P(LP)
+            print("Add/Comp")
+
+        elif conjType == "cont":
+            #Contrasting Conjunction
+            print("Cont")
+
+            #if LP == NP && RP == NP
+            #   P = P(RP)
+            #else
+            #   P = P(LP) + P(RP)
+            
+        
+    else:
+        #This status has no negation in it
+        print("No Negations in Status")
+        return 0
+    
+
+negationAnalyzer(stat0)
+negationAnalyzer(stat1)
+negationAnalyzer(stat2)
+negationAnalyzer(stat3)
+negationAnalyzer(stat4)
+negationAnalyzer(stat5)
+
+
+
+
+    
